579fae1c046f707025ac253d2fb7de7cfecc8706
asad <asadullahkhan.cse@gmail.com>
2019-02-05 15:16:48 +0530

    with Jinja templates

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -4,7 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEEDS = {'ent':'https://economictimes.indiatimes.com/small-biz/entrepreneurship/rssfeeds/11993034.cms',
+             'fin': 'https://economictimes.indiatimes.com/industry/banking/finance/rssfeeds/13358259.cms',
+             'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
